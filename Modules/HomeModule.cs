506b7e85ca810a582e6e13c38377816c8fde4a53
Jordan Mysliwiec and Marilyn Carlin <jordan+marilyn@>
2017-05-25 14:07:33 -0700

    add form to allow user input for Artist constructor, linked from index.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -22,10 +22,18 @@ namespace CD.Objects
         List<Artist> allArtists = Artist.GetAll();
         return View["artists.cshtml", allArtists];
       };
+      Get["/artists/new"] = _ => {
+        return View["artist_form.cshtml"];
+      };
       Post["/cds"] = _ => {
         CD newCD = new CD(Request.Form["cd-name"]);
         List<CD> allCDs = CD.GetAll();
         return View["cds.cshtml", allCDs];
+      };
+      Post["/artists"] = _ => {
+        Artist newArtist = new Artist(Request.Form["artist-name"]);
+        List<Artist> allArtists = Artist.GetAll();
+        return View["artists.cshtml", allArtists];
       };
     }
   }
