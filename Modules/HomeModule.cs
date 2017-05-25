373604d6a644963afc21642ba76174b399b87d3e
Jordan Mysliwiec and Marilyn Carlin <jordan+marilyn@>
2017-05-25 11:33:20 -0700

    add functionality to view list of CDs from link on index.cshtml.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -11,6 +11,10 @@ namespace CD.Objects
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
+      Get["/cds"] = _ => {
+        List<CD> allCDs = CD.GetAll();
+        return View["cds.cshtml", allCDs];
+      };
       Get["/cds/new"] = _ => {
         return View["cd_form.cshtml"];
       };
